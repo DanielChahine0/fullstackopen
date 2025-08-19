1de1863d4eab1d5212f0bef52e49f610d80e6f48
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-08-18 17:55:56 -0700

    Completed exercise 1.11

--- part1/unicaf/src/App.jsx
+++ part1/unicaf/src/App.jsx
@@ -28,12 +28,41 @@ const Statistics = ({ good, neutral, bad }) => {
     const positive = good / total * 100
     return (
       <div>
-        <StatisticLine text='good' value={good} />
+        {/* <StatisticLine text='good' value={good} />
         <StatisticLine text='neutral' value={neutral} />
         <StatisticLine text='bad' value={bad} />
         <StatisticLine text='all' value={total} />
         <StatisticLine text='average' value={average}/>
-        <StatisticLine text='positive' value={positive}/>
+        <StatisticLine text='positive' value={positive+"%"}/> */}
+
+        <table>
+          <tbody>
+            <tr>
+              <td>good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>all</td>
+              <td>{total}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{average}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{positive}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
 
@@ -69,6 +98,7 @@ const App = () => {
       <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
+    
   )
 }
 
