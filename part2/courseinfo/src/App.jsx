639536626cb7ac9b9cbbb58171e767066c19137e
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-08-25 12:37:05 -0700

    Update App.jsx

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,15 +1,12 @@
 const Header = (props) => {
-  console.log(props)
   return <h1>{props.course.name}</h1>
 }
 
 const Part = (props) => {
-  console.log(props)
   return <p>{props.part} {props.exercise}</p>
 }
 
 const Content = (props) => {
-  console.log(props);
   return (
     <div>
       <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
@@ -21,7 +18,6 @@ const Content = (props) => {
 
 
 const Total = (props) => {
-  console.log(props)
   return (
     <div>
       <p>
@@ -31,33 +27,38 @@ const Total = (props) => {
   )
 }
 
-
+const Course = (props) => {
+  // console.log(props)
+  return (
+    <div>
+      {/* <Header course={props.name}/> */}
+    </div>
+  )
+}
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
-        name:'Using props to pass data',
-        exercises: 7
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  console.log(course)
+  return <Course course={course} />
 }
 
 export default App
