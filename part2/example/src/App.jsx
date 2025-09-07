ec60abdc765e7b4d633031a0e6e5d5f66154b0ee
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-07 16:49:56 -0400

    updated the app

--- part2/example/src/App.jsx
+++ part2/example/src/App.jsx
@@ -45,7 +45,18 @@ const App = () => {
   const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
   
   const toggleImportanceOf = (id) => {
-    console.log('importance of '+ id + ' needs to be toggled')
+    const url = 'http://localhost:3001/notes/' + id
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+    
+    axios.put(url, changedNote).then(
+      (response) => {
+        setNotes(notes.map((note) => {
+          if (note.id === id) return response.data
+          else return note
+        }))
+      }
+    )
   }
 
   return (
