b89024971ccbcfd046db2949ff1042f771492212
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-08 13:27:04 -0400

    added a catch for fail safe toggle deleted notes

--- part2/example/src/App.jsx
+++ part2/example/src/App.jsx
@@ -28,6 +28,11 @@ const App = () => {
       returnedNotes => {
         setNotes(notes.map(note => note.id === id ? returnedNotes : note))
       }
+    ).catch(
+      error => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter(note => note.id !== id))
+      }
     )
   }
 
