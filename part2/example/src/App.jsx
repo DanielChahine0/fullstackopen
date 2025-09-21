208d6a16d9bae58f6b7daefd678ddb240c29597c
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-21 00:01:58 -0400

    Added a new notification component

--- part2/example/src/App.jsx
+++ part2/example/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   // Fetch the notes from the JSON server 
   useEffect(() => {
@@ -65,6 +67,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={()=> setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
