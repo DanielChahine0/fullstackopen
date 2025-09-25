e1ef8b7c27e04543a75a21dc1ee7d99346aaaca5
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-25 00:06:34 -0400

    created a filter

--- part2/coutries/src/App.jsx
+++ part2/coutries/src/App.jsx
@@ -1,4 +1,13 @@
 import { useState } from 'react'
+import countryService from './services/countries'
+
+const Filter = ({ value }) => {
+  return (
+    <div>
+      find countries
+    </div>
+  )
+}
 
 const App = () => {
   const [filter, setFilter] = useState('')
