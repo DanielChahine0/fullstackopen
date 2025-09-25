02cd01a03bbceac18ee0ab3e306e7269646eb8e6
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-25 00:20:40 -0400

    Update App.jsx

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import countryService from './services/countries'
 
-const Filter = ({ value }) => {
+const Filter = ({ value, onChange }) => {
   return (
     <div>
-      find countries
+      find countries <input value={value} onChange={onChange} />
     </div>
   )
 }
 
 const App = () => {
   const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const searchedCountries = countryService.getAll()
+    setCountries(searchedCountries);
+  }, [filter])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
   
+  // if (!filter){
+  //   return null
+  // }
+
   return (
     <div>
-
+      <Filter value={filter} onChange={handleFilterChange} />
+      {console.log(countries)}
     </div>
   )
 }
