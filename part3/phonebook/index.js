4edaecee3be56a7e337d86d2d3445953b5876eab
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-10-18 18:26:20 -0400

    Completed exercise 3.1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,9 +4,33 @@ const app = express()
 
 app.use(express.json())
 
+// ============== DATA ==============
+const persons = [
+    {
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    {
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    {
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    {
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
 // ============== ROUTES ==============
 app.get('/api/persons', (request, response) => {
-    response.send("HI")
+    response.json(persons)
 })
 
 // ============== START THE SERVER ==============
