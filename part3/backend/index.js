8b538f8c91eeac349f4da5addbb8e98ae9bc4357
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-10-22 23:33:56 -0400

    Update static file directory to 'dist'
    Changed the static file serving directory from 'build' to 'dist' to reflect the new output location. Also commented out the root route handler for '/' that returned a Hello World message.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 
+
 const requestLogger = (request, response, next) => {
     console.log("Method: ", request.method);
     console.log("Path: ", request.path);
@@ -16,7 +17,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors());
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 
 // ============== DATA ==============
@@ -49,9 +50,9 @@ const generateID = () => {
 
 
 // ============== ROUTES ==============
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>');
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World</h1>');
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
