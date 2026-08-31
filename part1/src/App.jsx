72cee3e9ad9782c96138e87d38a014d632ee2409
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-08-30 20:06:23 -0400

    part 1 - component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
+  const now = new Date();
+  const a = 20;
+  const b = 10;
+  console.log(now, a+b);
+  console.log("Hello from App component")
   return (
     <div>
       <p>Hello world</p>
+      <p>It is now {now.toString()}</p>
+      <p> {a} + {b} = {a+b}</p>
     </div>
   )
 }
