eadf80ecb80f3bc43e0139ba9d685d133482717a
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-08 14:01:41 -0400

    p1 Update of the state is asynchronous

--- part1/courselearning/src/App.jsx
+++ part1/courselearning/src/App.jsx
@@ -15,17 +15,21 @@ const Button = ({onClick, text}) => {
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const newLeft = left + 1
+    setLeft(newLeft)
+    setTotal(newLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    const newRight = right + 1
+    setRight(newRight)
+    setTotal(left + newRight)
   }
 
   return (
@@ -36,6 +40,7 @@ const App = () => {
       {right}
 
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
