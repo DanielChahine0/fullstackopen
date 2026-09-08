74090cd0190f9acbb2db5ac21876382970a75ee8
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-08 13:56:18 -0400

    p1 Handling arrays

--- part1/courselearning/src/App.jsx
+++ part1/courselearning/src/App.jsx
@@ -13,26 +13,31 @@ const Button = ({onClick, text}) => {
 }
 
 const App = () => {
-  const [clicks, setClicks] = useState({left:0, right:0})
-  const handleLeftClicks = () => {
-    setClicks({
-      ...clicks, 
-      left: clicks.left + 1
-    })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
-  const handleRightClicks = () => {
-    setClicks({
-      ...clicks, 
-      right: clicks.right + 1
-    })
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
+
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClicks}>left</button>
-      <button onClick={handleRightClicks}>right</button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
+
 export default App
