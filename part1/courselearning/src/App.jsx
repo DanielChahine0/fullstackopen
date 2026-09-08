c9f5ef8cbd84a5e678a26552314b9c7b5dba37a3
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-08 13:53:00 -0400

    p1 Complex state

--- part1/courselearning/src/App.jsx
+++ part1/courselearning/src/App.jsx
@@ -13,33 +13,26 @@ const Button = ({onClick, text}) => {
 }
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const [clicks, setClicks] = useState({left:0, right:0})
+  const handleLeftClicks = () => {
+    setClicks({
+      ...clicks, 
+      left: clicks.left + 1
+    })
   }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
+  const handleRightClicks = () => {
+    setClicks({
+      ...clicks, 
+      right: clicks.right + 1
+    })
   }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClicks}>left</button>
+      <button onClick={handleRightClicks}>right</button>
+      {clicks.right}
     </div>
   )
-} 
-
+}
 export default App
