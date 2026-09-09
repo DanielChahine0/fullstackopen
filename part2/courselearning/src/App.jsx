6419ae9b8934da2497599f692063612c686e8de9
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-09 13:02:18 -0400

    p2 Key-attribute

--- part2/courselearning/src/App.jsx
+++ part2/courselearning/src/App.jsx
@@ -5,7 +5,13 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note)=>{return <li>{note.content}</li>})}
+        {notes.map((note)=>{
+          return (
+            <li key={note.id}>
+              {note.content}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
