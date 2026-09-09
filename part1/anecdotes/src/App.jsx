84ebcbfdaa2cf42927cb61a8b5680b07552d13f4
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-09 00:19:39 -0400

    p1 1.12

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const DisplayVotes = ({selected, votes}) => {
+  let value = 0
+  if (selected in votes) {
+    value = votes[selected]
+  }
+  
+  return (
+    <div>
+      has {value} votes
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})
+
+  const changeSelected = () => {
+    const randomVal = Math.floor(Math.random() * anecdotes.length)
+    
+    setSelected(randomVal)
+  }
+
+  const vote = () => {
+    // Check if the selected has a vote already
+    if (Object.hasOwn(votes, selected)){
+      setVotes({
+        ...votes,
+       [selected]: votes[[selected]] + 1
+      })
+    }
+    else {
+      setVotes({
+        ...votes,
+        [selected]: 1
+      })
+    }
+  }
+
+  return (
+    <div>
+      <div>{anecdotes[selected]}</div>
+      <DisplayVotes selected={selected} votes={votes}/>
+      <Button text="next anecdote" onClick={changeSelected} />
+      <Button text="vote" onClick={vote}/>
+    </div>
+  )
+}
+
+export default App
