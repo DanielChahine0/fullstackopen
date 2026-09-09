67e37e5e296b067f585502d8b0cdfdcafe5cea4b
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-09 13:14:04 -0400

    p2 Refactoring Modules

--- part2/courselearning/src/App.jsx
+++ part2/courselearning/src/App.jsx
@@ -1,16 +1,12 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note)=>{
-          return (
-            <li key={note.id}>
-              {note.content}
-            </li>
-          )
+          return <Note key={note.id} note={note}/>
         })}
       </ul>
     </div>
