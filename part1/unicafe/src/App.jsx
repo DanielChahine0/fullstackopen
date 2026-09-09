aac6ee7269ae0f15def8777e6316ce3603ad4dec
Daniel Chahine <Chahinedaniel0@gmail.com>
2026-09-08 20:23:44 -0400

    p1 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -34,13 +34,42 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return (
     <div>
-      <StatisticsLine text="good" value={good} />
+      {/* <StatisticsLine text="good" value={good} />
       <StatisticsLine text="neutral" value={neutral} />
       <StatisticsLine text="bad" value={bad} />
 
       <StatisticsLine text="all" value={good+neutral+bad} />
       <StatisticsLine text="average" value={(good-bad)/(good+neutral+bad)} />
-      <StatisticsLine text="positive" value={(good)/(good+neutral+bad)} />
+      <StatisticsLine text="positive" value={(good)/(good+neutral+bad)} /> */}
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{good+neutral+bad}</td>
+          </tr>
+          <tr>
+            <td>avereage</td>
+            <td>{(good-bad)/(good+neutral+bad)}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{(good)/(good+neutral+bad)*100} %</td>
+          </tr>
+        </tbody>
+        
+      </table>
     </div>
   )
 }
